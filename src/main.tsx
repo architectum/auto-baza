421913c7e98674b664904611e8d82bc71dd1a273
🚀ivanets <evgeniyivanets@gmail.com>
2026-05-07 14:10:25 +0300

    remove pin lock

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,12 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { AppErrorBoundary, ErrorProvider } from './components/ErrorModal';
-import { PinLockProvider } from './components/PinLock';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppErrorBoundary>
       <ErrorProvider>
-        <PinLockProvider>
-          <App />
-        </PinLockProvider>
+        <App />
       </ErrorProvider>
     </AppErrorBoundary>
   </StrictMode>,
